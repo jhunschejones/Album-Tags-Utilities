c45e08f0c59acda2bf98a4345d7d02859ae17f8d
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-07-07 17:28:47 -0700

    Add all code used for AT3 migragion
    * Ruby script handles the data "pulling" and manipulation
    * Files in application code dir represent temporary changes to the app code itself to expose the endpoints needed to complete the migration

---
+++ album-tags-3-migration/application code/router.ex
@@ -0,0 +1,71 @@
+defmodule AlbumTagsWeb.Router do
+  use AlbumTagsWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    # plug :fetch_flash
+    plug :protect_from_forgery
+    # plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net/npm/vue 'unsafe-inline'; connect-src 'self'; img-src 'self' is1-ssl.mzstatic.com/image/ is2-ssl.mzstatic.com/image/ is3-ssl.mzstatic.com/image/ is4-ssl.mzstatic.com/image/ is5-ssl.mzstatic.com/image/ is6-ssl.mzstatic.com/image/ is7-ssl.mzstatic.com/image/ is8-ssl.mzstatic.com/image/ is9-ssl.mzstatic.com/image/; style-src 'self' 'unsafe-inline' fonts.googleapis.com/icon; font-src 'self' fonts.gstatic.com/s/materialicons/;"}
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src * 'unsafe-eval' 'unsafe-inline'"}
+    plug NavigationHistory.Tracker, history_size: 6 # limit to 6 entries needed to redirect prior to login sequence
+    plug AlbumTagsWeb.AuthPlug
+  end
+
+  pipeline :browser_no_csrf do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    # plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net/npm/vue 'unsafe-inline'; connect-src 'self'; img-src 'self' is1-ssl.mzstatic.com/image/ is2-ssl.mzstatic.com/image/ is3-ssl.mzstatic.com/image/ is4-ssl.mzstatic.com/image/ is5-ssl.mzstatic.com/image/ is6-ssl.mzstatic.com/image/ is7-ssl.mzstatic.com/image/ is8-ssl.mzstatic.com/image/ is9-ssl.mzstatic.com/image/; style-src 'self' 'unsafe-inline' fonts.googleapis.com/icon; font-src 'self' fonts.gstatic.com/s/materialicons/;"}
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src * 'unsafe-eval' 'unsafe-inline'"}
+    plug NavigationHistory.Tracker, history_size: 6 # limit to 6 entries needed to redirect prior to login sequence
+    plug AlbumTagsWeb.AuthPlug
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AlbumTagsWeb do
+    pipe_through :browser
+
+    get "/", StaticPageController, :home
+    get "/tags/search/:search_string", ListController, :tag_search
+    resources "/albums", AlbumController, only: [:show]
+    resources "/tags", TagController, only: [:create, :edit, :delete]
+    resources "/connections", ConnectionController, only: [:new, :create, :edit, :delete]
+    resources "/lists", ListController, only: [:index, :show, :new, :create, :edit, :update, :delete]
+  end
+
+  scope "/api", AlbumTagsWeb do
+    pipe_through :api
+
+    get "/apple/search/", AppleMusicController, :search
+    get "/apple/details/:id", AppleMusicController, :details
+  end
+
+  scope "/migrate", AlbumTagsWeb do
+    pipe_through :api
+
+    post "/album", MigrationController, :create_album
+    post "/tag", MigrationController, :add_tag
+    post "/connection", MigrationController, :add_connection
+    post "/list", MigrationController, :create_list
+  end
+
+  scope "/auth", AlbumTagsWeb do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
+  # Ignore csrf tokens for logout rout only. This prevents users from seeing an
+  # error if they are authenticated in two windows, log out in one, then try to
+  # log out in the second.
+  scope "/auth", AlbumTagsWeb do
+    pipe_through :browser_no_csrf
+
+    delete "/logout", AuthController, :logout
+  end
+end
---
+++ album-tags-3-migration/script.rb
@@ -0,0 +1,170 @@
+# This script requires both a locally running Album Tags 2 Staging (port 3000)
+# and an Album Tags 3 Dev (port 4000)
+require 'httparty'
+require 'dotenv/load'
+
+USER_CONVERSION_HASH = {
+  "xxxxxx" => 123,
+  "xxxxxx" => 456
+}
+
+@trouble_records = []
+
+def get_all_albums
+  HTTParty.get("http://localhost:3000/api/v1/album",
+    :body => { "apiToken": ENV["API_TOKEN"] }.to_json,
+    :headers => { 'Content-Type' => 'application/json' }
+  )
+end
+
+def get_list_data(list_id)
+  JSON.parse(
+    HTTParty.get("http://localhost:3000/api/v1/list/#{list_id}").body
+  )
+end
+
+def get_favorites(user_id)
+  favorites = {}
+  favorites[user_id] = JSON.parse(
+    HTTParty.get("http://localhost:3000/api/v1/favorite/#{user_id}").body
+  )
+
+  favorites
+end
+
+def get_all_list_ids(all_albums)
+  all_lists = []
+  all_albums.each do |album|
+    if album["lists"].length > 0
+      album["lists"].each do |list|
+        all_lists.push(list["id"])
+      end
+    end
+  end
+
+  all_lists.sort.uniq
+end
+
+def get_all_list_data(all_list_ids)
+  all_list_data = []
+  all_list_ids.each do |list_id|
+    all_list_data.push(get_list_data(list_id))
+  end
+
+  all_list_data
+end
+
+def get_all_user_favorites
+  all_favorites = []
+  USER_CONVERSION_HASH.each do |old_id, new_id|
+    all_favorites.push(get_favorites(old_id))
+  end
+
+  all_favorites.compact
+end
+
+def send_album_to_AT3(album)
+  # === Get or create album by apple_album_id ===
+  response = HTTParty.post('http://localhost:4000/migrate/album', body: { apple_album_id: album["appleAlbumID"] })
+  return @trouble_records.push(album["appleAlbumID"]) if response.code == 500
+
+  album_id = JSON.parse(response.body)["album_id"]
+
+  # === Filter tags down to users being migrated ===
+  album["tags"] = album["tags"].select { |tag| USER_CONVERSION_HASH[tag["creator"]] }
+
+  if album["tags"].length > 0
+    # === Create tags ===
+    album["tags"] = album["tags"].map do |tag|
+      {
+        text: tag["text"],
+        user_id: USER_CONVERSION_HASH[tag["creator"]],
+        custom_genre: tag["customGenre"]
+      }
+    end
+
+    # === Associate tags to album ===
+    album["tags"].each do |tag|
+      HTTParty.post('http://localhost:4000/migrate/tag',
+        body: { text: tag[:text], user_id: tag[:user_id], custom_genre: tag[:custom_genre], album_id: album_id }.to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    end
+  end
+
+  # === Filter connections down to users being migrated ===
+  album["connections"] = album["connections"].select { |conn| USER_CONVERSION_HASH[conn["creator"]] }
+
+  # === Create connections ===
+  if album["connections"].length > 0
+    album["connections"] = album["connections"].map do |conn|
+      {
+        parent_album: conn["albumOne"],
+        child_album: conn["albumTwo"],
+        user_id: USER_CONVERSION_HASH[conn["creator"]]
+      }
+    end
+
+    album["connections"].each do |conn|
+      HTTParty.post('http://localhost:4000/migrate/connection',
+        body: conn.to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+    end
+  end
+  # progress marker
+  print '.'
+end
+
+def send_list_to_AT3(list_title:, user_id:, albums:)
+  HTTParty.post('http://localhost:4000/migrate/list',
+    body: { title: list_title, user_id: user_id, albums: albums }.to_json,
+    headers: { 'Content-Type' => 'application/json' }
+  )
+  # progress marker
+  print '.'
+end
+
+def migrate_all_albums(all_albums)
+  all_albums.each do |album|
+    send_album_to_AT3(album)
+  end
+end
+
+def migrate_all_lists(all_lists)
+  all_lists.each do |list|
+    if USER_CONVERSION_HASH[list["user"]]
+      send_list_to_AT3(
+        list_title: list["title"],
+        user_id: USER_CONVERSION_HASH[list["user"]],
+        albums: list["albums"].map { |album| album["appleAlbumID"] }
+      )
+    end
+  end
+end
+
+def migrate_all_user_favorites(all_user_favorites)
+  all_user_favorites.each do |user_favorites|
+    send_list_to_AT3(
+      list_title: "My Favorites",
+      user_id: USER_CONVERSION_HASH[user_favorites.keys[0]],
+      albums: user_favorites.values[0].map { |album| album["appleAlbumID"] }
+    )
+  end
+end
+
+all_albums = get_all_albums
+migrate_all_albums(all_albums)
+
+all_list_data = get_all_list_data(get_all_list_ids(all_albums))
+migrate_all_lists(all_list_data)
+
+all_user_favorites = get_all_user_favorites
+migrate_all_user_favorites(all_user_favorites)
+
+# finish progress meter
+puts '.'
+puts "\nMigration complete!\n"
+
+# report errors
+puts "REPORT: there was a problem with one or more albums: #{@trouble_records.join(", ")}" if @trouble_records.length > 0
